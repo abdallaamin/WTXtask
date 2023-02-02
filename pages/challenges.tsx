0312e2d394af914160be6060a4bb1702ef51b8c7
abdalla-amin <abdalla.amin@vodafone.com>
2023-02-02 04:30:43 +0200

    modal dynamic

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -9,7 +9,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 const faq = [
     {
         id: 1,
-        question: "what is your name?",
+        question: "what is your name b2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa?",
         answer:"amin",
     },
     {
@@ -25,49 +25,26 @@ interface FaqProps {
 export default function Faq() {
     return (
         <div>
-            <Accordion>
+            {
+                faq.map((f) => (
+            <div>
+            <Accordion key={f.id}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                 >
-                    <Typography>Accordion 1</Typography>
+                    <Typography>{f.question}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        {f.answer}
                     </Typography>
                 </AccordionDetails>
             </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Typography>Accordion 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
+            </div>
+                ))
+            }
         </div>
     );
 }
-// {
-//     faq.map((f) => (
-//         <ExpansionPanel key={f.id}>
-//             <ExpansionPanelSummary
-//             >
-//                 <Typography>{f.question}</Typography>
-//             </ExpansionPanelSummary>
-//             <ExpansionPanelDetails>
-//                 <Typography>{f.answer}</Typography>
-//             </ExpansionPanelDetails>
-//         </ExpansionPanel>
-//     ))
-// }
