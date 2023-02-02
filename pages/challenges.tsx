39dc8b62a975a2f32dfb3336e34bff6e0b14e84c
abdalla-amin <abdalla.amin@vodafone.com>
2023-02-02 04:27:41 +0200

    challenge page done

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -1,6 +1,9 @@
-import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 const faq = [
@@ -22,19 +25,49 @@ interface FaqProps {
 export default function Faq() {
     return (
         <div>
-            {faq.map((f) => (
-                <ExpansionPanel key={f.id}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                    >
-                        <Typography>{f.question}</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <Typography>{f.answer}</Typography>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            ))}
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
+                >
+                    <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
     );
 }
-
+// {
+//     faq.map((f) => (
+//         <ExpansionPanel key={f.id}>
+//             <ExpansionPanelSummary
+//             >
+//                 <Typography>{f.question}</Typography>
+//             </ExpansionPanelSummary>
+//             <ExpansionPanelDetails>
+//                 <Typography>{f.answer}</Typography>
+//             </ExpansionPanelDetails>
+//         </ExpansionPanel>
+//     ))
+// }
